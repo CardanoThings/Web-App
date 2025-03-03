e31ffdad46c229cf88bcb97fbf4a8acfcf7cc96f
Mad Orkestra <mad@madorkestra.com>
2025-03-03 23:36:45 +0000

    Update

---
+++ src/lib/Code.svelte
@@ -0,0 +1,36 @@
+<script>
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/atom-one-dark.css'; // or another style
+	import { onMount } from 'svelte';
+	import { on } from 'svelte/events';
+
+	export let language = 'xml';
+	let highlightedCode;
+	let codeBlock;
+	onMount(() => {
+		// clean up the code block by removing leading and trailing white spaces
+		codeBlock.innerHTML = codeBlock.innerHTML.replace(/^\s+|\s+$/g, '');
+
+		highlightedCode = hljs.highlight(codeBlock.innerHTML, {
+			language: 'xml'
+		}).value;
+	});
+</script>
+
+<pre class="hide">
+	<code bind:this={codeBlock}>
+		<slot></slot>
+	</code>
+</pre>
+
+<pre class="hljs">
+	<code class={language}>
+	{@html highlightedCode}
+</code>
+</pre>
+
+<style>
+	.hide {
+		display: none;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Code from '$lib/Code.svelte';
+</script>
+
+<Code language="html">
+	<span>Hello World</span>
+	<span>Hello World</span>
+	<span>Hello World</span>
+</Code>
