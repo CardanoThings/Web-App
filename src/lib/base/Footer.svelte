ece589463864ce3f22a7634ad79440dc931324d6
Mad Orkestra <mad@madorkestra.com>
2025-03-04 23:54:50 +0000

    Footer

---
+++ src/lib/base/Footer.svelte
@@ -0,0 +1,7 @@
+<footer class="border-t-2 border-black dark:border-white">
+	<section
+		class="m-auto mt-4 flex items-center justify-between px-4 py-4 sm:w-[95%] sm:px-4 md:w-[90%] lg:w-[80%] xl:w-[70%] 2xl:w-[60%]"
+	>
+		<p class="text-xs">&copy; 2025 CardanoThings. All rights reserved.</p>
+	</section>
+</footer>
