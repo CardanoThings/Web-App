e698a14a40bce12eeebdcfb8eb95cef98988866d
Mad Orkestra <mad@madorkestra.com>
2025-10-31 19:25:28 +0100

    Layout Updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script>
 	import ESP32 from '$lib/ESP32.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
 	import SectionWorkshops from '$lib/SectionWorkshops.svelte';
 	import SectionIntroductions from '$lib/SectionIntroductions.svelte';
-	import SectionHardware from '$lib/SectionHardware.svelte';
 	import SectionBlocks from '$lib/SectionBlocks.svelte';
-	import SectionGlossary from '$lib/SectionGlossary.svelte';
 	let { data } = $props();
 </script>
 
@@ -12,7 +11,7 @@
   <ESP32 modelUrl="/models/esp32c6.gltf" />
 </div> -->
 
-<section class="flex">
+<section class="flex w-3/4">
 	<h1 class="mb-12 text-8xl font-thin text-white">Cardano meets the Internet of Things.</h1>
 </section>
 
@@ -24,14 +23,33 @@
 	<SectionIntroductions />
 </div>
 
-<div class="mb-16 w-full border-b border-dashed border-white">
-	<SectionHardware />
-</div>
-
-<div class="mb-16 w-full border-b border-dashed border-white">
+<div class="mb-8 w-full border-b border-dashed border-white">
 	<SectionBlocks />
 </div>
 
-<div class="mb-16 w-full border-b border-dashed border-white">
-	<SectionGlossary />
+<div class="mb-8 flex border-b border-dashed border-white">
+	<section id="blocks" class="mb-8 w-1/2">
+		<div class="mb-4 text-white">
+			<h1 class="text-3xl">Glossary</h1>
+			<p class="mt-2 max-w-[90%] text-lg font-thin">
+				A collection of terms and definitions related to all the tech used on CardanoThings.io.
+			</p>
+		</div>
+		<Button href="/glossary" class="bg-white text-black transition-all hover:bg-zinc-200"
+			>View Full Glossary</Button
+		>
+	</section>
+
+	<section id="hardware" class="mb-8 w-1/2">
+		<div class="mb-4 text-white">
+			<h1 class="text-3xl">Hardware</h1>
+			<p class="mt-2 max-w-[90%] text-lg font-thin">
+				Essential hardware components and development boards for building IoT projects on
+				CardanoThings.io
+			</p>
+		</div>
+		<Button href="/hardware" class="bg-white text-black transition-all hover:bg-zinc-200"
+			>View Hardware List</Button
+		>
+	</section>
 </div>
--- src/routes/hardware/+page.svelte
+++ src/routes/hardware/+page.svelte
@@ -6,15 +6,15 @@
 </script>
 
 <section class="mb-16 flex flex-col gap-4 text-white">
-	<h1 class="text-6xl font-medium">Hardware</h1>
-	<p class="max-w-[90%] text-lg font-thin">
+	<h1 class="w-1/2 text-6xl font-medium">Hardware</h1>
+	<p class="w-1/2 text-lg font-thin">
 		Essential hardware components and development boards for building IoT projects on
 		CardanoThings.io
 	</p>
 
 	<div class="mt-8 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
 		{#each data.hardware as item}
-			<Card.Root>
+			<Card.Root class="gap-2">
 				<Card.Header>
 					<Card.Title class="mt-0 pt-0 text-xl">{item.name}</Card.Title>
 					<Card.Description class="leading-6">
@@ -22,7 +22,7 @@
 					</Card.Description>
 				</Card.Header>
 				<Card.Content class="flex-1"></Card.Content>
-				<Card.Footer>
+				<Card.Footer class="mt-0 pt-0">
 					<Button href={item.link} size="sm">Learn More</Button>
 				</Card.Footer>
 			</Card.Root>
